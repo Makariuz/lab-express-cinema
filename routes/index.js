4ef9aa8b60f0c24fec71123cbd1fa9b443e39609
Makariuz <samuel.eduardo23@hotmail.com>
2022-01-28 11:17:19 +0000

    basically solved

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,53 @@
+
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose')
+const Movie = require('./../models/movie.model')
+const hbs = require('hbs')
+
+
+//concat
+hbs.registerHelper('concat', function () {
+    let outStr = ''
+    for (let arg in arguments) {
+      if (typeof arguments[arg] != 'object') {
+        outStr += arguments[arg]
+      }
+    }
+    return outStr
+  })
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+//movies route
+router.get('/movies', async (req,res) => {
+    const movies = await getMovies()
+  // sending the posts page
+  res.render('movies', {movies})
+})
+
+//function to getMovies
+async function getMovies() {
+    try {
+      const movies = await Movie.find()
+      return movies
+    } catch (error) {
+      console.log(error)
+      return []
+    }
+  }
+
+//route for movieID
+router.get('/movie/:id', async (req,res) => {
+    const id = req.params.id
+    // find the post in mongobd
+    const movie = await Movie.findById(id)
+    const stars = await Movie.find()
+    const showtimes = await Movie.find()
+    res.render('seeMore', { id, movie, stars, showtimes })
+    console.log(movie)
+    console.log(id)
+})
 
 module.exports = router;
--- seeds/movies.seed.js
+++ seeds/movies.seed.js
@@ -1,3 +1,5 @@
+// To insert in "seeds/movies.seed.js"
+
 const movies = [
     {
       title: "A Wrinkle in Time",
@@ -80,46 +82,9 @@ const movies = [
       showtimes: ["13:00", "15:30", "18:00", "20:10", "22:40"]
     }
   ];
+  
+  // Add here the script that will be run to actually seed the database (feel free to refer to the previous lesson)
+  
+  // ... your code here
 
-// ℹ️ Gets access to environment variables/settings
-// https://www.npmjs.com/package/dotenv
-require('dotenv/config');
-
-// ℹ️ Connects to the database
-require('./db');
-
-// Handles http requests (express is node js framework)
-// https://www.npmjs.com/package/express
-const express = require('express');
-
-// Handles the handlebars
-// https://www.npmjs.com/package/hbs
-const hbs = require('hbs');
-
-/* //get the movie model 
-const Movie = require('./models/movie.model')
-
-movies.forEach(async (movie) => {
-    await Movie.create(movie)
-}) */
-
-const app = express();
-
-// ℹ️ This function is getting exported from the config folder. It runs most middlewares
-require('./config')(app);
-
-// default value for title local
-const projectName = 'lab-express-cinema';
-const capitalized = string => string[0].toUpperCase() + string.slice(1).toLowerCase();
-
-app.locals.title = `${capitalized(projectName)}- Generated with Ironlauncher`;
-
-
-// 👇 Start handling routes here
-const index = require('./routes/index');
-app.use('/', index);
-
-// ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
-require('./error-handling')(app);
-
-module.exports = app;
+  
